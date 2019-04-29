5126a8bcb3f086f3a53750bc1a0cbcdb916c89b8
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2019-04-29 22:54:44 +0300

    created route in yaml, best practices

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -3,14 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class WeatherController extends AbstractController
 {
-    /**
-     * @Route("/weather", name="weather")
-     */
-    public function index()
+    public function index(): Response
     {
         return $this->render('weather/index.html.twig', [
             'controller_name' => 'WeatherController',
