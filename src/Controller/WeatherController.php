b834014127b0d2086ca92b1ff6be3e29e48bbdc9
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2019-04-30 10:05:32 +0300

    Call not real Google API from a controller, prepare output

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,15 +2,29 @@
 
 namespace App\Controller;
 
+use App\ExternalApi\GoogleApi;
+use App\Model\NullWeather;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class WeatherController extends AbstractController
 {
-    public function index(): Response
+    public function index()
     {
+        try {
+            $fromGoogle = new GoogleApi();
+            $weather = $fromGoogle->getToday();
+        } catch (\Exception $exp) {
+            $weather = new NullWeather();
+        }
+
         return $this->render('weather/index.html.twig', [
             'controller_name' => 'WeatherController',
+            'weatherData' => [
+                'date'      => $weather->getDate()->format('Y-m-d'),
+                'dayTemp'   => $weather->getDayTemp(),
+                'nightTemp' => $weather->getNightTemp(),
+                'sky'       => $weather->getSky()
+            ],
         ]);
     }
 }
