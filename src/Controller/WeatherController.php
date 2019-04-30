02b16f661a640592acc831bb1f9a5830a7702416
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2019-04-30 18:40:01 +0300

    reverted all examples, lets use simple autowiring for now

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,20 +2,21 @@
 
 namespace App\Controller;
 
+use App\ExternalApi\GoogleApi;
 use App\Model\NullWeather;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class WeatherController extends Controller
+class WeatherController extends AbstractController
 {
     /**
      * @param           $day
+     * @param GoogleApi $googleApi
      * @return Response
      */
-    public function index($day): Response
+    public function index($day, GoogleApi $googleApi): Response
     {
         try {
-            $googleApi = $this->get('app.external_api.google_api');
             $weather = $googleApi->getDay(new \DateTime($day));
         } catch (\Exception $exp) {
             $weather = new NullWeather();
