34152e9be0cd44d64620ce683a903118ac6a7c43
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2019-04-30 14:38:44 +0300

    Injected FilesystemCache component into Loader Service

--- src/Weather/LoaderService.php
+++ src/Weather/LoaderService.php
@@ -4,18 +4,25 @@ namespace App\Weather;
 
 use App\ExternalApi\GoogleApi;
 use App\Model\Weather;
+use Symfony\Component\Cache\Simple\FilesystemCache;
 
 class LoaderService
 {
     /** @var GoogleApi */
     private $weatherService;
+
+    /** @var FilesystemCache */
+    private $cacheService;
+
     /**
      * LoaderService constructor.
-     * @param GoogleApi $weatherService
+     * @param GoogleApi       $weatherService
+     * @param FilesystemCache $cacheService
      */
-    public function __construct(GoogleApi $weatherService)
+    public function __construct(GoogleApi $weatherService, FilesystemCache $cacheService)
     {
         $this->weatherService = $weatherService;
+        $this->cacheService = $cacheService;
     }
     /**
      * @param \DateTime $day
