fa06045086440bfab7fa592249def7756bc8a394
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2019-04-30 11:24:26 +0300

    se autowiring! via dependency injection via constructor

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -11,21 +11,32 @@ use Symfony\Component\HttpFoundation\Response;
 
 class WeatherController extends AbstractController
 {
+    /** @var GoogleApi */
+    private $googleApi;
+
+    /**
+     * WeatherController constructor.
+     * @param GoogleApi $googleApi
+     */
+    public function __construct(GoogleApi $googleApi)
+    {
+        $this->googleApi = $googleApi;
+    }
+
     /**
      * @param           $day
-     * @param GoogleApi $googleApiService
      * @return Response
      */
-    public function index($day, GoogleApi $googleApiService): Response
+    public function index($day): Response
     {
         try {
-            $weather = $googleApiService->getDay(new DateTime($day));
+            $weather = $this->googleApi->getDay(new DateTime($day));
         } catch (Exception $exp) {
             $weather = new NullWeather();
         }
 
         return $this->render('weather/index.html.twig', [
-            'weatherData'     => [
+            'weatherData' => [
                 'date'      => $weather->getDate()->format('Y-m-d'),
                 'dayTemp'   => $weather->getDayTemp(),
                 'nightTemp' => $weather->getNightTemp(),
