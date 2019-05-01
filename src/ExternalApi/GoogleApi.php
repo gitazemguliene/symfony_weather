e7ba7d5eb3e5912f57aaf78dabc699505a7c5d1e
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2019-05-01 22:07:56 +0300

    added provider name to weather model into each provider class

--- src/ExternalApi/GoogleApi.php
+++ src/ExternalApi/GoogleApi.php
@@ -2,7 +2,22 @@
 
 namespace App\ExternalApi;
 
+use App\Model\Weather;
+
 class GoogleApi extends AbstractWeatherService implements WeatherApiInterface
 {
     public const PROVIDER_NAME = 'Google';
+
+    /**
+     * @param \DateTime $day
+     * @return Weather
+     * @throws \Exception
+     */
+    public function getDay(\DateTime $day): Weather
+    {
+        $today = parent::getDay($day);
+        $today->setProviderName(self::PROVIDER_NAME);
+
+        return $today;
+    }
 }
