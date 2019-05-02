45ca6e00fabb4ba00e44643c3ed1de406eac4fe8
gitazemguliene <34140545+gitazemguliene@users.noreply.github.com>
2019-05-02 18:30:11 +0300

    created abstract class and included into API's classes

--- src/ExternalApi/GoogleApi.php
+++ src/ExternalApi/GoogleApi.php
@@ -2,39 +2,8 @@
 
 namespace App\ExternalApi;
 
-use App\Model\NullWeather;
-use App\Model\Weather;
-
-class GoogleApi implements WeatherApiInterface
+class GoogleApi extends AbstractWeatherService implements WeatherApiInterface
 {
     public const PROVIDER_NAME = 'Google';
-    /**
-     * @param \DateTime $day
-     * @return Weather
-     * @throws \Exception
-     */
-    public function getDay(\DateTime $day): Weather
-    {
-        $today = $this->load(new NullWeather());
-        $today->setDate($day);
 
-        return $today;
-    }
-
-    /**
-     * @param Weather $before
-     * @return Weather
-     * @throws \Exception
-     */
-    private function load(Weather $before): Weather
-    {
-        $now = new Weather();
-        $base = $before->getDayTemp();
-        $now->setDayTemp(random_int(5 - $base, 5 + $base));
-        $base = $before->getNightTemp();
-        $now->setNightTemp(random_int(-5 - abs($base), -5 + abs($base)));
-        $now->setSky(random_int(1, 3));
-
-        return $now;
-    }
 }
